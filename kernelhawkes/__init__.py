a53d311d1df7b5ff38c0e8f90763a505dac6312e
Maxime Sangnier <maxime.sangnier@sorbonne-universite.fr>
2025-02-14 11:19:10 +0100

    Second commit

---
+++ kernelhawkes/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1"
+
+from .kernelhawkes import HawkesProcess, MultivariateKernelHawkes, MultivariateExponentialHawkes, plot_kernels
+
+
+__all__ = ['HawkesProcess', 'MultivariateKernelHawkes', 'MultivariateExponentialHawkes', 'plot_kernels']
